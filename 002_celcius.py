223eb7ef64d39e7bb7666316a5f252e66ccdc2a0
Vijaykumarreddy1992 <vjkumar.pm@gmail.com>
2025-05-29 23:25:30 +0530

    vjkumar

---
+++ 002_celcius.py
@@ -0,0 +1,4 @@
+# Converting Fahrenheit to Celsius
+fahrenheit =int(input("Enter the temperature in Fahrenheit: "))
+celsius = (5 / 9) * (fahrenheit - 32)
+print(f"Temperature in Celsius is {celsius}")
